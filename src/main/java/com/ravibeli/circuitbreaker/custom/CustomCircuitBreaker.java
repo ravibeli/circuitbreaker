63965ca7e1239515649186c0827202647f66bced
Ravikumar Beli <Ravikumar.Beli@blueyonder.com>
2021-07-11 19:11:26 +0530

    page name correction removed spring retry bean

--- src/main/java/com/ravibeli/circuitbreaker/custom/CustomCircuitBreaker.java
+++ src/main/java/com/ravibeli/circuitbreaker/custom/CustomCircuitBreaker.java
@@ -1,25 +1,10 @@
-package com.ravibeli.circuitbreaker.config;
-
-
-import java.util.function.Function;
-import java.util.function.Supplier;
-import org.springframework.cloud.client.circuitbreaker.CircuitBreaker;
-
+package com.ravibeli.circuitbreaker.custom;
 /**
- * @author ravibeli@gmail.com
  * @project circuitbreaker
- * @created on 11 Jul, 2021 4:31 PM
+ * @author Ravikumar.Beli@blueyonder.com
+ * @created on 11 Jul, 2021 7:04 PM
+ *
  **/
 
-public class CustomCircuitBreaker implements CircuitBreaker {
-
-    @Override
-    public <T> T run(Supplier<T> toRun) {
-        return CircuitBreaker.super.run(toRun);
-    }
-
-    @Override
-    public <T> T run(Supplier<T> toRun, Function<Throwable, T> fallback) {
-        return toRun.get();
-    }
+public class CustomCircuitBreaker {
 }
--- src/main/java/com/ravibeli/circuitbreaker/custom/CustomCircuitBreakerFactory.java
+++ src/main/java/com/ravibeli/circuitbreaker/custom/CustomCircuitBreakerFactory.java
@@ -1,30 +1,10 @@
-package com.ravibeli.circuitbreaker.config;
-
-import java.util.function.Function;
-import org.springframework.cloud.client.circuitbreaker.CircuitBreaker;
-import org.springframework.cloud.client.circuitbreaker.CircuitBreakerFactory;
-import org.springframework.cloud.client.circuitbreaker.ConfigBuilder;
-
+package com.ravibeli.circuitbreaker.custom;
 /**
- * @author ravibeli@gmail.com
  * @project circuitbreaker
- * @created on 11 Jul, 2021 4:31 PM
+ * @author Ravikumar.Beli@blueyonder.com
+ * @created on 11 Jul, 2021 7:05 PM
+ *
  **/
 
-public class CustomCircuitBreakerFactory extends CircuitBreakerFactory {
-
-    @Override
-    public CircuitBreaker create(String id) {
-        return new CustomCircuitBreaker();
-    }
-
-    @Override
-    protected ConfigBuilder configBuilder(String id) {
-        return null;
-    }
-
-    @Override
-    public void configureDefault(Function defaultConfiguration) {
-
-    }
+public class CustomCircuitBreakerFactory {
 }
